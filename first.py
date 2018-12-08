3ed7a0a6c2fe1f27fbd55587c49e0b7383606bac
klos100 <anna.jalowiecka@gmail.com>
2018-12-08 11:28:43 +0100

    init

---
+++ first.py
@@ -0,0 +1,7 @@
+a ="1"
+print (a)
+print (a+ str(1))
+print (type(None))
+print (1**2)
+
+help(str)
---
+++ zadanie_1.py
@@ -0,0 +1,31 @@
+# policz pole trójkąta o podstawie 10 i wysokosci 5
+
+podstawa = 10
+wysokosc = 5
+wynik = (podstawa * wysokosc)/2
+
+print ("Pole trojkąta wyniosi: ", wynik)
+
+# policz pole kola o promieniu 7
+
+promien = 7
+wynik = 3.14*promien**2
+
+print ("Pole koła wyniosi: ", wynik)
+
+# policz pole trapezu o dlugosci podstaw 3 i 9 oraz wysokosci 5
+
+podstawa1 = 3
+podstawa2 = 9
+wysokosc = 5
+wynik = ((podstawa1+podstawa2)* wysokosc)/2
+
+print ("Pole trapezu wyniosi: ", wynik)
+
+# policz objetosc kuli o zadanym promieniu
+
+r = 2
+
+wynik = 4/3*3.14*r**3
+
+print ("Objetosc kuli o zadanym promieniu wyniosi: ", wynik)
